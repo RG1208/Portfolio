4d9e3da98a59ba7b02e8822af35be9f4a7e5a213
RG1208 <rachitgarg1208@gmail.com>
2025-05-10 15:40:19 +0000

    fix: Images Path

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -96,7 +96,7 @@ const Hero: React.FC = () => {
               <div className="absolute -inset-1 bg-gradient-to-r from-primary-600 to-secondary-600 rounded-2xl blur opacity-20 dark:opacity-40"></div>
               <div className="relative overflow-hidden rounded-2xl border-2 border-gray-200 dark:border-gray-700 shadow-xl bg-white dark:bg-gray-800">
                 <img
-                  src="public/Images/RachitGarg.jpg"
+                  src="https://res.cloudinary.com/dx2ttgkba/image/upload/v1746891560/RachitGarg_jidjc1.jpg"
                   alt="Rachit Garg"
                   className="w-full h-80 object-cover object-center sm:h-96"
                 />
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     title: 'Agri Oracle',
     description: 'AgriOracle is a smart agriculture platform that integrates modern AI/ML models with a user-friendly web interface. It helps farmers and stakeholders by providing features like crop disease detection, Next crop Recommendation, and intelligent insights through a MERN stack application connected with Flask-based AI APIs.',
     technologies: ['React', 'Node.js', 'TypeScript','Flask', 'TensorFlow'],
-    image: 'public/Images/AgriOracle_IMG.jpg',
+    image: 'https://res.cloudinary.com/dx2ttgkba/image/upload/v1746891561/AgriOracle_IMG_kg9qjf.jpg',
     githubUrl: 'https://github.com/RG1208/AgriOracle',
     // demoUrl: 'https://demo.example.com',
     featured: true,
