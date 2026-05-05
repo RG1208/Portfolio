825bf5645f6c9d2392239cd619ad4d61e7c67815
Rachit Garg <rachitgarg1208@gmail.com>
2026-05-05 14:45:28 +0530

    feat: Patent Achievement added

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -9,8 +9,17 @@ export interface Achievement {
 }
 
 export const achievements: Achievement[] = [
-  {
+    {
     id: 1,
+    title: 'Patent Published - Vehicle Impact Detection System',
+    event: 'Indian Patent Office',
+    date: '2026',
+    description: 'Published a patent titled "Vehicle Impact Detection and Emergency Response Device", focusing on real-time accident detection, automated emergency alert generation, and rapid response coordination. The system also integrates blockchain-based mechanisms for secure and tamper-proof incident data recording, enhancing reliability and transparency in emergency situations.',
+    imageUrl: 'https://drive.google.com/file/d/19CH-w1kE5bA57HY7Jmch1xNmDS8LvTEr/view?usp=sharing',
+    certificateUrl: 'https://drive.google.com/file/d/19CH-w1kE5bA57HY7Jmch1xNmDS8LvTEr/view?usp=sharing',
+  },
+  {
+    id: 2,
     title: '2nd Place - BizTech Ideathon',
     event: 'NDIM Business School Ideathon Competition',
     date: 'September 2025',
@@ -19,7 +28,7 @@ export const achievements: Achievement[] = [
     certificateUrl: 'https://res.cloudinary.com/dx2ttgkba/image/upload/v1757854370/WhatsApp_Image_2025-09-14_at_18.22.34_ecxefl.jpg',
   },
   {
-    id: 2,
+    id: 3,
     title: '4th Place - IILM Ideathon 2025',
     event: 'IILM University Gurugram',
     date: 'November 2025',
